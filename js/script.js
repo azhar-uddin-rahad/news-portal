7ed7c63760e5fd86886687203e8dff54859d00ef
azharrahad31 <azherudinrahad@gmail.com>
2023-05-31 23:25:25 +0600

    data load form api

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-fetch('')
+fetch('https://openapi.programming-hero.com/api/news/categories')
+.then(res => res.json())
+.then(data =>displayData(data.data.news_category))
+
+
+
+const displayData=(data)=>{
+    console.log(data)
+    const ul=document.querySelector('.ul');
+
+    data.forEach(categoric => {
+        ul.innerHTML += `<li onclick="${loadData(categoric.category_id)}" > ${categoric.category_name}</li>`;
+        
+    });
+}
+
+const loadData=(id)=>{
+    console.log(id)
+}
